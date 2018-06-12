112705e8bd2baf8e47c44dbb8ea65d1d98a0e1e6
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-06-13 02:59:50 +0700

    🔧 enable binaryen-loader with default configuration - current default config   - optimizeLevel: "-O2"   - shrinkLevel: "-Os"   - debugInfo: false   - passes (DefaultGlobalOptimization passes):     - duplicate-function-elimination     - inlining-optimizing     - remove-unused-module-elements     - memory-packing

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,8 @@ module.exports = {
         use: [{
           loader: 'wasm-loader'
         }, {
-          loader: 'rust-native-wasm-loader',
+          loader: 'binaryen-loader'
+        }, { loader: 'rust-native-wasm-loader',
           options: {
             release: process.env.NODE_ENV === 'production',
             gc: process.env.NODE_ENV === 'production'
