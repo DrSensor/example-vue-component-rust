a92055360a37f9c60081d03f98ec98587b485d64
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-06-14 15:23:51 +0700

    🔧 enable post-emscripten and remove-memory passes

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,17 @@ module.exports = {
         use: [{
           loader: 'wasm-loader'
         }, {
-          loader: 'binaryen-loader'
-        }, { loader: 'rust-native-wasm-loader',
+          loader: 'binaryen-loader',
+          options: {
+            transformation: {
+              passes: [
+                'post-emscripten',
+                'remove-memory'
+              ]
+            }
+          }
+        }, {
+          loader: 'rust-native-wasm-loader',
           options: {
             release: process.env.NODE_ENV === 'production',
             gc: process.env.NODE_ENV === 'production'
